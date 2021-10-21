b5a9f78e5fc7441bddaa4a386a6229d7adaf1187
by-German <german280599@gmail.com>
2021-10-21 00:28:36 -0500

    chore: configurate tomcat, jenkis, applicatiom and app.properties

--- src/main/java/com/frikiteam/events/EventsApplication.java
+++ src/main/java/com/frikiteam/events/EventsApplication.java
@@ -3,10 +3,17 @@ package com.frikiteam.events;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class EventsApplication {
+public class EventsApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EventsApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(EventsApplication.class, args);
